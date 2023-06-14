385c738c3d5cfece976e8bfed3c338af947e4561
huangmiaojie <firemao96@gmail.com>
2023-06-15 00:21:39 +0800

    初始化项目配置

---
+++ src/page/vueUse.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1> 测试 vueUse 的鼠标坐标 </h1>
+    <h3>Mouse: {{ x }} x {{ y }}</h3>
+</template>
+    
+<script lang="ts" setup>
+import { defineComponent } from 'vue';
+import { useMouse } from '@vueuse/core'
+import request from '@/utils/axios';
+const requestRes = async () => {
+    let result = await request({
+        url: '/api/xxx',
+        method: 'get'
+    });
+}
+
+const { x, y } = useMouse()
+</script>
+    
